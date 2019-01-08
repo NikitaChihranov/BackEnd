8663693f14441dd36ec9dcdb13df563aef011e5c
nikitaChihranov <nikita.chihranov@gmail.com>
2019-01-08 14:21:57 +0400

    Version 8.01.2019
    Signed-off-by: nikitaChihranov <nikita.chihranov@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,10 @@ let PaymentRouter = require('./paymentRouter');
 let ProducerRouter = require('./producerRouter');
 let UserRouter = require('./userRouter');
 let AboutRouter = require('./aboutRouter');
-let PhotoRouter = require('./photoRouter');
 
 router.use('/products', ProductRouter);
 router.use('/categories', CategoryRouter);
 router.use('/comments', CommentRouter);
-router.use('/photos', PhotoRouter)
 router.use('/orders', OrderRouter);
 router.use('/payments', PaymentRouter);
 router.use('/producers', ProducerRouter);
--- routes/photoRouter.js
+++
@@ -1,7 +0,0 @@
-let router = require('express').Router();
-let PhotoController = require('../controllers/photoController');
-
-router.get('/', PhotoController.get);
-router.post('/', PhotoController.create);
-
-module.exports = router;
