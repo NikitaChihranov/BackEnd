8a3f1dc5f5328eea8e8c8729927e2b9438d912e0
nikitaChihranov <nikita.chihranov@gmail.com>
2019-02-04 23:23:43 +0400

    Add case when photo of user doesn`t exist.
    Signed-off-by: nikitaChihranov <nikita.chihranov@gmail.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,8 +18,7 @@ let upload = multer({
 let controller = {};
 controller.signin = async (req, res, next) => {
     if (req.user.firstName === 'not found') {
-        console.log('Incorrect login or password');
-        let user = new User({ firstName: 'not found'});
+        let user = new User({firstName: 'not found'});
         res.status(200).json(user);
     } else {
         res.status(200).json(req.user);
@@ -28,117 +27,117 @@ controller.signin = async (req, res, next) => {
 controller.getAll = async (req, res, next) => {
     try {
         res.status(200).json(await User.find({}));
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
 controller.getById = async (req, res, next) => {
-    try{
+    try {
         let id = req.params.id;
         let user = await User.findById(id);
         res.status(200).json(user);
-    }catch (e) {
-        next(new ControllerError (e.message, 400));
+    } catch (e) {
+        next(new ControllerError(e.message, 400));
     }
 };
 controller.create = async (req, res, next) => {
-    try{
-        if(req.body.login!==''&&req.body.password!=='') {
+    try {
+        if (req.body.login !== '' && req.body.password !== '') {
             let alreadyExists = await User.findOne({login: req.body.login});
             if (alreadyExists) {
                 let user = new User({firstName: 'Already exists!'});
                 res.status(201).json(user);
             } else {
                 let user = await User.create(req.body);
-                res.status(201).json(user);
+                if (user.photo == undefined) {
+                    user.photo = 'no photo';
+                    user.save();
+                    console.log(user);
+                    res.status(201).json(user);
+                } else {
+                    res.status(201).json(user);
+                }
             }
-        }
-        else{
+        } else {
             let user = new User({firstName: 'err'});
             res.status(201).json(user);
         }
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
 controller.createAdmin = async (req, res, next) => {
-    try{
+    try {
         let user = await User.create(req.body);
         user.admin = true;
         user.save();
         console.log('user works' + user);
         res.status(201).json(user);
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
 controller.uploadPhoto = async (req, res, next) => {
     let user = await User.findOne({_id: req.params.id});
-    if(user) {
-        upload(req, res, (err) => {
-            if (err) console.log(err);
-                let photo = req.file.filename;
-                user.photo = photo;
-                user.save();
-                res.status(200).json(user);
-        })
-    }else{
-        user.photo = 'Photo doesn`t exist';
+    upload(req, res, (err) => {
+        if (err) console.log(err);
+        let photo = req.file.filename;
+        user.photo = photo;
         user.save();
-        console.log('User: ' + user);
-        res.status(201).json(user);
-    }
-}
+        res.status(200).json(user);
+    })
+};
 controller.update = async (req, res, next) => {
-    try{
+    try {
         let userWithPhoto = await User.findOne({_id: req.params.id});
         fs.unlink('./public/userPhotos/' + userWithPhoto.photo, (err) => (err));
         let user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
         res.status(200).json(user);
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
 controller.updatePhoto = async (req, res, next) => {
     let user = await User.findOne({_id: req.params.id});
-    try{
+    try {
         upload(req, res, (err) => {
-            if(err) console.log(err);
+            if (err) console.log(err);
             user.photo = req.file.filename;
             user.save();
-            res.status(200).json(user); 
+            res.status(200).json(user);
         })
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 }
 controller.delete = async (req, res, next) => {
-    try{
-        let userWithPhoto = await User.findOne({_id:req.params.id});
+    try {
+        let userWithPhoto = await User.findOne({_id: req.params.id});
         fs.unlink('./public/userPhotos/' + userWithPhoto.photo, (err) => (err));
         let user = await User.findByIdAndRemove(req.params.id);
         res.status(200).json(user);
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
 controller.deleteAll = async (req, res, next) => {
     try {
         let users = await User.find({});
-        for(let user of users) {
+        for (let user of users) {
             fs.unlink('./public/userPhotos/' + user.photo, (err) => (err));
         }
-        res.json(await User.deleteMany({}, (err) => {}));
+        res.json(await User.deleteMany({}, (err) => {
+        }));
     } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 }
 controller.isAuthenticated = async (req, res, next) => {
     console.log(req.user);
-        if (req.user) {
-            next();
-        } else {
-            console.log('You dont have roots');
-        }
+    if (req.user) {
+        next();
+    } else {
+        console.log('You dont have roots');
+    }
 }
 module.exports = controller;
